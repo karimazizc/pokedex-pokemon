e48cf933144c4eeb40d1cd63399084b2a77d1961
karimazizc <karimazizc@gmail.com>
2025-08-12 20:28:08 +0700

    changed the base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pokedex-pokemon/';
+  base: '/pokedex-pokemon/',
   plugins: [
     vue(),
     vueDevTools(),
